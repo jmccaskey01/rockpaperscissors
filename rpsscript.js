3bb083b54392172528da9810fe3e5032b4c7a4e4
Jordan McCaskey <jordan.mccaskey@gmail.com>
2019-05-31 17:18:11 -0400

    Refactored computerSelection function

--- rpsscript.js
+++ rpsscript.js
@@ -1,14 +1,15 @@
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3);
 
-  if (choice == 0) {
-    return "rock";
-  } else if (choice == 1) {
-    return "paper";
-  } else if (choice == 2) {
-    return "scissors";
+  switch (choice) {
+    case 0: return "rock";
+    break;
+    case 1: return "paper";
+    break;
+    case 2: return "scissors";
   }
 }
+
 function playerPlay() {
   return prompt("Rock, Paper, or Scissors?", "Choose your weapon.").toLowerCase();
 }
