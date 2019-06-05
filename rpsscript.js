c065fc0c570b94d09d0cdf21a24b9e8444024a97
Jordan McCaskey <jordan.mccaskey@gmail.com>
2019-06-05 15:35:38 -0400

    added eventListeners to buttons

--- rpsscript.js
+++ rpsscript.js
@@ -10,9 +10,10 @@ function computerPlay() {
   }
 }
 
-function playerPlay() {
-//listen for button click, return that button click
-}
+//deprecated - wipe when done
+//function playerPlay() {
+//  return prompt("Rock, Paper, or Scissors?", "Choose your weapon.").toLowerCase();
+//}
 
 function compareSelection(playerSelection, computerSelection) {
   //return 1 if player wins, return 2 if computer wins, return 0 if tie?
@@ -42,6 +43,9 @@ function compareSelection(playerSelection, computerSelection) {
   }
 }
 
+function playRound(playerSelection) {
+  return compareSelection(playerSelection, computerPlay());
+}
 
 function game() {
   let computerSelection;
@@ -49,25 +53,44 @@ function game() {
   let playerScore = 0;
   let computerScore = 0;
   let roundResult;
+  /*
+  1. listen for button press
+  2. on button press, call playRound, passing playerSelection
+  3. playRound will return result as described in compareSelection: 1 for player wins round, 2 for computer wins round, 0 for tie
+  4. store result in score tally, display on scoreboard - remember to bui
+  5. announce winner once one side gets 5 points
+  */
+  //loop for five rounds
+
+  /* LISTENERS */
+  const buttons = document.querySelectorAll('button')
+
+  buttons.forEach(button) => {
+      btn.addEventListener('click', playRound(button.id));
+  }
 
 
-  computerSelection = computerPlay();
-  playerSelection = playerPlay();
 
-  console.log(`Round: ${round}`);
-  roundResult = compareSelection(playerSelection, computerSelection);
+  for (let round = 1; round <= 5; round++) {
+    computerSelection = computerPlay();
+    playerSelection = playerPlay();
 
-  if (roundResult == 1) {
-    playerScore++;
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You won this round.`);
-    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
-  } else if (roundResult == 2) {
-    computerScore++;
-    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You lost this round.`);
-    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
-  } else if (roundResult == 0) {
-    console.log('It was a tie.');
-    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+    console.log(`Round: ${round}`);
+    roundResult = compareSelection(playerSelection, computerSelection);
+
+    if (roundResult == 1) {
+      playerScore++;
+      console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You won this round.`);
+      console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+    } else if (roundResult == 2) {
+      computerScore++;
+      console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You lost this round.`);
+      console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+    } else if (roundResult == 0) {
+      console.log('It was a tie.');
+      console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+    }
+
   }
 
   console.log(`The final score is: Player: ${playerScore} Computer:${computerScore}`);
@@ -78,7 +101,7 @@ function game() {
   } else {
     console.log("The whole game was a tie.");
   }
-
+  //display winner and score after 5 rounds
 }
 
 game();
