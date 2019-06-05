f9e64dd19f4e3dfc449dfe6dd2face3fca407694
Jordan McCaskey <jordan.mccaskey@gmail.com>
2019-06-05 16:34:34 -0400

    got buttons playing rounds

--- rpsscript.js
+++ rpsscript.js
@@ -1,23 +1,28 @@
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let computerSelection;
+let playerSelection;
+
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3);
 
   switch (choice) {
-    case 0: return "rock";
+    case 0: return computerSelection = "rock";
     break;
-    case 1: return "paper";
+    case 1: return computerSelection = "paper";
     break;
-    case 2: return "scissors";
+    case 2: return computerSelection = "scissors";
   }
 }
 
-//deprecated - wipe when done
-//function playerPlay() {
-//  return prompt("Rock, Paper, or Scissors?", "Choose your weapon.").toLowerCase();
-//}
-
+// REFACTOR
 function compareSelection(playerSelection, computerSelection) {
   //return 1 if player wins, return 2 if computer wins, return 0 if tie?
   //refactor with ? operator?
+  //I trust integers more than strings I guess
+  //can be refactored a lot of ways, just ensure function takes two choices for
+  //the game, compares them, returns the winner in a consistent way
   if (playerSelection == computerSelection) {
     return 0;
   } else if (playerSelection == "rock") {
@@ -43,19 +48,38 @@ function compareSelection(playerSelection, computerSelection) {
   }
 }
 
-function playRound(playerSelection) {
+function playRound(buttonID) {
+  playerSelection = buttonID;
   return compareSelection(playerSelection, computerPlay());
 }
 
+function updateScoreboard(roundResult) {
+
+  round++;
+  console.log(`Round: ${round}`);
+
+  if (roundResult == 1) {
+    playerScore++;
+    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You won this round.`);
+    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+  } else if (roundResult == 2) {
+    computerScore++;
+    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. You lost this round.`);
+    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+  } else if (roundResult == 0) {
+    console.log(`You chose ${playerSelection}. The computer chose ${computerSelection}. It was a tie.`);
+    console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
+  }
+}
+
+//core function
 function game() {
-  let computerSelection;
-  let playerSelection;
-  let playerScore = 0;
-  let computerScore = 0;
-  let roundResult;
+
+  //let roundResult;
+
   /*
-  1. listen for button press
-  2. on button press, call playRound, passing playerSelection
+  1. listen for button press DONE
+  2. on button press, call playRound, passing playerSelection DONE
   3. playRound will return result as described in compareSelection: 1 for player wins round, 2 for computer wins round, 0 for tie
   4. store result in score tally, display on scoreboard - remember to bui
   5. announce winner once one side gets 5 points
@@ -63,15 +87,19 @@ function game() {
   //loop for five rounds
 
   /* LISTENERS */
-  const buttons = document.querySelectorAll('button')
+  const buttons = document.querySelectorAll('button');
 
-  buttons.forEach(button) => {
-      btn.addEventListener('click', playRound(button.id));
-  }
+  buttons.forEach((button) => {
+    //  console.log(`clicked ${button.id}`)
+    // updateScoreboard(round, playRound(button.id))
+      button.addEventListener('click', () => {
+      updateScoreboard(playRound(button.id));
+      });
+  });
 
 
 
-  for (let round = 1; round <= 5; round++) {
+  /* for (let round = 1; round <= 5; round++) {
     computerSelection = computerPlay();
     playerSelection = playerPlay();
 
@@ -91,8 +119,8 @@ function game() {
       console.log(`The current score is: Player:${playerScore}  Computer:${computerScore}`);
     }
 
-  }
-
+  }*/
+  /*
   console.log(`The final score is: Player: ${playerScore} Computer:${computerScore}`);
   if (playerScore > computerScore) {
     console.log("You won!");
@@ -100,7 +128,7 @@ function game() {
     console.log("You lost!");
   } else {
     console.log("The whole game was a tie.");
-  }
+  }*/
   //display winner and score after 5 rounds
 }
 
